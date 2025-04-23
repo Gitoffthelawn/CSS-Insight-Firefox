773a75be020268f38d88d18a9a24bba47b6d721b
MarkAlexI <markovaleksandr108@gmail.com>
2025-04-23 10:08:46 +0300

    Update info about captured rules.

--- scripts/getDeclaredStyles.js
+++ scripts/getDeclaredStyles.js
@@ -40,7 +40,7 @@ function getDeclaredStyles(selector = 'body') {
       for (const rule of rules) {
         for (const _selector of subSelectors) {
           if (checkRule(rule, _selector)) {
-            result += `${FOUND} "${_selector}": \n`;
+            result += `${FOUND} "${_selector}"(${rule.parentStyleSheet?.href}): \n`;
             result += formatRule(rule) + '\n\n';
           }
         }
