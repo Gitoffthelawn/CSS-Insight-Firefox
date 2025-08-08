dd882470c991aa4fac4d15bf0c77207786b481f3
MarkAlexI <markovaleksandr108@gmail.com>
2025-08-08 19:37:07 +0300

    fixed: an error with opening the updates page

--- scripts/service_worker.js
+++ scripts/service_worker.js
@@ -1,3 +1,14 @@
+const showUpdates = (details) => {
+  if (details.reason === 'update') {
+    const version = browser.runtime.getManifest().version;
+    browser.tabs.create({
+      url: browser.runtime.getURL(`update/update.html?version=${version}`)
+    });
+  }
+};
+
+browser.runtime.onInstalled.addListener(showUpdates);
+
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'close_current_tab') {
     if (sender.tab && sender.tab.id) {
